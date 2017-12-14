0ba94f9b1a127c171a363c2c5a77d1a0821a3116
Atmaja Handyana <ATMH@ESG-GMBH.DE>
2017-12-14 16:04:04 +0100

    Add Angular Login-Register Component

---
+++ collection-apps/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginRegisterComponent } from './login-register/login-register.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginRegisterComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- collection-apps/src/app/app.module.ts
+++ collection-apps/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { LoginRegisterComponent } from './login-register/login-register.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
+    LayoutModule,
 
-    LayoutModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
