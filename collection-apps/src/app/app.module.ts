3cc5227a9a7a661b3157361c136a9689731e8aab
Atmaja Handyana <ATMH@ESG-GMBH.DE>
2017-12-21 14:32:07 +0100

    Add Material Design & Login/Register Site

--- collection-apps/src/app/app.module.ts
+++ collection-apps/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { LoginRegisterComponent } from './login-register/login-register.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -16,10 +18,11 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
+    NoopAnimationsModule,
     LayoutModule,
-
-    AppRoutingModule
+    NgbModule.forRoot(),
+    AppRoutingModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
